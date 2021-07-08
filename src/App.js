946a7e7641b238b9a1f4ca8cda1dc0aaf190e2ee
Tala Edaly <talaedaly4@gmail.com>
2021-07-08 20:55:16 +0300

    tala

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> abood
+          Edit <code>src/App.js</code> tala
         </p>
         <a
           className="App-link"
